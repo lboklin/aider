bdbf4b2f090c74b1b4a4e9295f08927bac6b0a87
Quinlan Jager <quinlan.jager@copiawealthstudios.com>
2025-05-08 10:21:42 -0700

    Update tool prompting to be more direct

--- aider/coders/base_coder.py
+++ aider/coders/base_coder.py
@@ -1188,8 +1188,6 @@ class Coder:
 
     def fmt_system_prompt(self, prompt):
         final_reminders = []
-        if self.mcp_tools and len(self.mcp_tools) > 0:
-            final_reminders.append(self.gpt_prompts.tool_prompt)
 
         if self.main_model.lazy:
             final_reminders.append(self.gpt_prompts.lazy_prompt)
@@ -1224,6 +1222,9 @@ class Coder:
             )
         else:
             quad_backtick_reminder = ""
+
+        if self.mcp_tools and len(self.mcp_tools) > 0:
+            final_reminders.append(self.gpt_prompts.tool_prompt)
 
         final_reminders = "\n\n".join(final_reminders)
 
